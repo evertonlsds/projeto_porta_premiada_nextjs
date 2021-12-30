77b3885ceb437d1f37ab07550126c4fc4b204dc4
Everton santos <everton0102@gmail.com>
2021-12-30 20:44:11 -0300

    Pagina jogo ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,9 @@ import {criarPortas, atualizarPortas} from "../functions/portas"
 
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(6, 2))
-
-  function renderizarPortas(){
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value = {porta} 
-        onChange = {novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
-    })
-  }
-  
   return (
     <div style ={{display: "flex"}}>
-      {renderizarPortas()}
+      <h1>Inicio do Jogo</h1>
       </div>
   )
 }
